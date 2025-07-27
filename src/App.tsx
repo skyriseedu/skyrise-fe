3e56ed563b2a2243ad4eb862053b8895dc25872c
KyiThantSin <kyits485@gmail.com>
2025-07-27 22:34:46 +0700

    chore: update font size classes in App component and enhance index.css with utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-primary text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-primary text-h1 font-bold underline">Hello world!</h1>
+      <p className="text-body-1">Loream asperam</p>
     </>
   );
 }
