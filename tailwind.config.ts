994df544be40594d701154d2883e92fb0594b5e3
KyiThantSin <kyits485@gmail.com>
2025-07-27 21:05:58 +0700

    chore: add Tailwind CSS configuration file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,3 +10,5 @@ export default {
   },
   plugins: [],
 }
+
+export default config
