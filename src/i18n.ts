ba5e055be603301044694737d5f349d35c5a7bdc
KyiThantSin <kyits485@gmail.com>
2025-08-01 13:11:15 +0700

    refactor: update i18n import statements and remove unused type definitions

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
-import i18n from 'i18next';
+import i18nModule from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languageDetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import enTranslation from './locales/en/translation.json';
 import myTranslation from './locales/my/translation.json';
 
@@ -14,6 +14,8 @@ const resources = {
     translation: myTranslation,
   },
 };
+
+const i18n = i18nModule as any;
 
 i18n
   // detect user language
