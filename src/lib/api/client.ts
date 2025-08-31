7fa4dedf49abc835555b9eb11e98994c7ce6e125
SawZiDunn <szidunn199@gmail.com>
2025-08-31 22:43:24 +0700

    update: change base api

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  'http://skyrise-backend-wh9o.onrender.com/api/v1';
+  'https://skyrise-backend-wh9o.onrender.com/api/v1';
 
 // axios instance
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
+  withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
   },
