a76e54aa3243a2a2dce640f7f9ca39816073b976
KyiThantSin <kyits485@gmail.com>
2025-09-02 01:09:31 +0700

    fix: reduce font size in desktop

--- src/components/common/StickyHeader.tsx
+++ src/components/common/StickyHeader.tsx
@@ -44,7 +44,7 @@ const StickyHeader: React.FC<StickyHeaderProps> = ({
             )}
             <div className="flex-1">
               {title && (
-                <h1 className="text-text-primary text-xl leading-tight font-semibold lg:text-2xl">
+                <h1 className="text-text-primary text-lg leading-tight font-semibold lg:text-h2">
                   {title}
                 </h1>
               )}
