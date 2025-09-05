bb17b469edde2bc091a16a40ccf248dfc5c5735a
KyiThantSin <kyits485@gmail.com>
2025-09-05 15:49:35 +0700

    chore: update folder structure

---
+++ src/components/services/consultation/ServicesShowcase.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ServiceCard from '@/components/services/consultation/ServiceCard';
+import admissionSupportIcon from '@/assets/admission-support-home.svg';
+import visaSupportIcon from '@/assets/visa-support-home.svg';
+import preUniversityHome from '@/assets/pre-university-home.svg';
+import airpotPickUp from '@/assets/airport-pickup-home.svg';
+
+const ServicesShowcase: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="mb-6 text-h3 lg:text-h2 font-semibold">Other services you may want to check</h1>
+
+      <div className="flex gap-4 overflow-x-auto pb-2 mt-3 md:grid md:grid-cols-4 md:gap-6 md:overflow-visible">
+        <div className="shrink-0 md:shrink md:w-auto">
+          <ServiceCard
+            title="Admission Process Support"
+            icon={admissionSupportIcon}
+          />
+        </div>
+        <div className="shrink-0 md:shrink md:w-auto">
+          <ServiceCard title="Visa Assistance" icon={visaSupportIcon} />
+        </div>
+        <div className="shrink-0 md:shrink md:w-auto">
+          <ServiceCard title="Accommodation & Airport Pick-up" icon={airpotPickUp} />
+        </div>
+        <div className="shrink-0 md:shrink md:w-auto">
+          <ServiceCard title="Pre-University Programs" icon={preUniversityHome} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesShowcase;
--- src/pages/users/services/CounselingPage.tsx
+++ src/pages/users/services/CounselingPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ServicesShowcase from '@/components/services/consultation/ServicesShowcase';
 
 const CounselingPage: React.FC = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="mb-4 text-3xl font-bold">Study Abroad Counseling</h1>
-      <p>Coming soon...</p>
+
+      <ServicesShowcase />
     </div>
   );
 };
