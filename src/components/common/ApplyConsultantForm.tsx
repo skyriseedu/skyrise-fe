a8d6759e6e8f02f244b75058ffce0d14734af4be
SawZiDunn <szidunn199@gmail.com>
2025-09-22 11:22:27 +0700

    fix: fix close btn issue

--- src/components/common/ApplyConsultantForm.tsx
+++ src/components/common/ApplyConsultantForm.tsx
@@ -55,7 +55,7 @@ const ConsultantForm: React.FC<ConsultantFormProps> = ({
       <button
         type="button"
         onClick={onClose}
-        className="bg-primary absolute -top-3 -right-3 z-10 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full text-white hover:bg-red-600"
+        className="bg-primary absolute -top-3 right-0 z-10 flex h-8 w-8 cursor-pointer items-center justify-center rounded-full text-white hover:bg-red-600"
         aria-label="Close"
       >
         <img
