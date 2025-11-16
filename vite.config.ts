d9ddc10c60ad842b386bc0a1f48e1a91b1ca56b1
Nyan Lin Aung <150507886+blaze-nyan@users.noreply.github.com>
2025-11-16 18:29:24 +0700

    added image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,69 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      includePublic: true,
+      logStats: true,
+
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+              },
+            },
+          },
+          'removeViewBox',
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+
+      png: {
+        quality: 90,
+        compressionLevel: 9,
+      },
+
+      jpeg: {
+        quality: 85,
+        progressive: true,
+        mozjpeg: true,
+      },
+
+      jpg: {
+        quality: 85,
+        progressive: true,
+        mozjpeg: true,
+      },
+
+      webp: {
+        quality: 85,
+        lossless: false,
+      },
+
+      avif: {
+        quality: 80,
+        lossless: false,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
