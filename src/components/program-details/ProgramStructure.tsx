a565c5d8ed7f56b916a3cfe9b21fb94294fba071
KyiThantSin <kyits485@gmail.com>
2026-02-10 23:14:01 +0700

    chore: update text in program structure

--- src/components/program-details/ProgramStructure.tsx
+++ src/components/program-details/ProgramStructure.tsx
@@ -37,7 +37,7 @@ const ProgramStructure: React.FC<ProgramStructureProps> = ({
       ? [
           {
             id: 'requirements',
-            title: 'Undergraduate Entry Requirements',
+            title: 'Entry Requirements',
             htmlContent: undergraduateEntryRequirement,
           } as AccordionItem,
         ]
